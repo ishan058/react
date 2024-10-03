6c9d54b6c642db0ac05b08ec900a8cfbd42cbc2c
ishan058 <ishankhadkaik246@gmail.com>
2024-10-03 12:00:23 +0545

    toast

---
+++ application/src/components/Toast.js
@@ -0,0 +1,13 @@
+// src/components/Toast.js
+import React from 'react';
+import './Toast.css';
+
+const Toast = ({ message, type }) => {
+    return (
+        <div className={`toast ${type}`}>
+            {message}
+        </div>
+    );
+};
+
+export default Toast;
