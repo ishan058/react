4e32d50fb5ab54a4eceecc41c848d47bcc1827cb
ishan058 <ishankhadkaik246@gmail.com>
2024-10-09 20:01:17 +0545

    scs

--- application/src/pages/About.js
+++ application/src/pages/About.js
@@ -3,10 +3,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="container">
+    <div>
       <h1>About Us</h1>
-      <p>Welcome to the About page of our application. Here you can learn more about what we do.</p>
-      <p>Our mission is to provide excellent products and services to our customers.</p>
+      <p>Welcome to the About page of our application.</p>
     </div>
   );
 };
