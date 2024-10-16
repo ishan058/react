61e8b27295f0c407b02596060fdd582072c46e12
ishan058 <ishankhadkaik246@gmail.com>
2024-10-16 20:47:51 +0545

    slices

---
+++ hamrobiruwa/src/slices/productsSlice.js
@@ -0,0 +1,37 @@
+// src/slices/productsSlice.js
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchProducts as fetchProductsAPI } from '../api';
+
+export const fetchProducts = createAsyncThunk(
+  'products/fetchProducts',
+  async () => {
+    const products = await fetchProductsAPI();
+    return products;
+  }
+);
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState: {
+    products: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default productsSlice.reducer;
